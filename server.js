51e44beb5b17fc78a3a2765bda232ed041f68fc3
jbds <sales@jbds.co.uk>
2020-07-02 14:36:22 +0100

    add isRebootVisible

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ let gameState = {
   handVisible: {north: true, east: true, south: true, west: true},
   isBiddingCycle: false,
   isBiddingHideDenominationButtons: true,
+  isRebootVisible: true,
   lastAction: "None(fromServer)",
   pack: [],
   pointOfCompassAndPlayers: [],
